89535ad5bcad93ae1acfbf8f8e339dd756d97066
DanielWLam <lwd931227@126.com>
2016-07-24 11:05:57 +0800

    增删不再整个列表渲染，只进行局部dom操作

--- app/js/index.js
+++ app/js/index.js
@@ -2,7 +2,9 @@
 'use strict';
 const electron = require('electron');
 let fs = require('fs')
-const { ipcRenderer } = electron;
+const {
+	ipcRenderer
+} = electron;
 
 let closeEl = document.querySelector('.close');
 let wrap = document.querySelector('.wrap');
@@ -16,198 +18,229 @@ let todoData = '';
 
 
 
-fs.readFile('./app/data.json',{encoding:'UTF-8'},function(err,data){
-		if(err) throw err;
-		todoData = JSON.parse(data);
-		renderList();
+fs.readFile('./app/data.json', {
+	encoding: 'UTF-8'
+}, function(err, data) {
+	if (err) throw err;
+	todoData = JSON.parse(data);
+	renderList();
 })
 
 function renderList() {
-    let _html = ''
-    for (let i = 0, len = todoData.length; i < len; i++) {
-        let curArr = todoData[i];
-        switch (curArr.priority) {
-            case 'A':
-                _html += '<li class="item priA fadeInDown animated"><i class="circle thin icon red"></i>' + curArr.name + '</li>';
-                break;
-            case 'B':
-                _html += '<li class="item priB fadeInDown animated"><i class="circle thin icon yellow"></i>' + curArr.name + '</li>';
-                break;
-            case 'C':
-                _html += '<li class="item priC fadeInDown animated"><i class="circle thin icon blue"></i>' + curArr.name + '</li>';
-                break;
-            case 'D':
-                _html += '<li class="item priD fadeInDown animated"><i class="circle thin icon green"></i>' + curArr.name + '</li>';
-        }
-    }
-    list.innerHTML = _html;
+	let _html = ''
+	for (let i = 0, len = todoData.length; i < len; i++) {
+		let curArr = todoData[i];
+		switch (curArr.priority) {
+			case 'A':
+				_html += '<li class="item priA fadeInDown animated"><i class="circle thin icon red"></i>' + curArr.name + '</li>';
+				break;
+			case 'B':
+				_html += '<li class="item priB fadeInDown animated"><i class="circle thin icon yellow"></i>' + curArr.name + '</li>';
+				break;
+			case 'C':
+				_html += '<li class="item priC fadeInDown animated"><i class="circle thin icon blue"></i>' + curArr.name + '</li>';
+				break;
+			case 'D':
+				_html += '<li class="item priD fadeInDown animated"><i class="circle thin icon green"></i>' + curArr.name + '</li>';
+		}
+	}
+	list.innerHTML = _html;
 }
 
 
-closeEl.addEventListener('click',function(){
+closeEl.addEventListener('click', function() {
 	ipcRenderer.send('close-main-window');
 })
-controlbar.addEventListener('click',function(e){
+controlbar.addEventListener('click', function(e) {
 	let target = null;
 	let iconNode = null;
 	let allI = controlbar.querySelectorAll('i');
 	let allItem = list.querySelectorAll('.item');
 
-	if(e.target.className=='tabChoose'){
+	if (e.target.className == 'tabChoose') {
 		target = e.target;
 	}
-	if(e.target.parentNode.className=='tabChoose'){
+	if (e.target.parentNode.className == 'tabChoose') {
 		target = e.target.parentNode;
 	}
 	iconNode = target.querySelector('.circle');
-	if(iconNode.className.indexOf('thin')>-1){
+	if (iconNode.className.indexOf('thin') > -1) {
 		let firstClick = true;
-		for(let i=0,len=allI.length;i<len;i++){
-			if(allI[i].className.indexOf('thin')<=-1){
+		for (let i = 0, len = allI.length; i < len; i++) {
+			if (allI[i].className.indexOf('thin') <= -1) {
 				allI[i].className += ' thin';
 			}
 		}
-		iconNode.className = iconNode.className.replace('thin','');
-	}else{
+		iconNode.className = iconNode.className.replace('thin', '');
+	} else {
 		let firstClick = false;
-		for(let i=0,len=allI.length;i<len;i++){
-			if(allI[i].className.indexOf('thin')<=-1){
+		for (let i = 0, len = allI.length; i < len; i++) {
+			if (allI[i].className.indexOf('thin') <= -1) {
 				allI[i].className += ' thin';
 			}
 		}
 	}
 
 	let priority = target.id;
-	if(firstClick){
-		for(let i=0,len=allItem.length;i<len;i++){
-			allItem[i].style.display='none';
-			if(allItem[i].className.indexOf('item '+priority)>-1){
-				allItem[i].style.display='block';
+	if (firstClick) {
+		for (let i = 0, len = allItem.length; i < len; i++) {
+			allItem[i].style.display = 'none';
+			if (allItem[i].className.indexOf('item ' + priority) > -1) {
+				allItem[i].style.display = 'block';
 			}
 		}
-	}else{
-		for(let i=0,len=allItem.length;i<len;i++){
-			allItem[i].style.display='block';
+	} else {
+		for (let i = 0, len = allItem.length; i < len; i++) {
+			allItem[i].style.display = 'block';
 		}
 	}
-},false)
+}, false)
 
-list.addEventListener('click',function(e){
-	let index = getIndex(list,'.circle',e.target);
-	if(e.target.className.indexOf('circle')>-1){
-		e.target.className = e.target.className.replace('thin','');
+list.addEventListener('click', function(e) {
+	let index = getIndex(list, '.circle', e.target);
+	if (e.target.className.indexOf('circle') > -1) {
+		e.target.className = e.target.className.replace('thin', '');
 	}
-	
-	if(index>-1){
-		todoData.splice(index,1);
-		fs.writeFile('./app/data.json',JSON.stringify(todoData),function(err,data){
-			if(err) throw err;
+
+	if (index > -1) {
+		todoData.splice(index, 1);
+		fs.writeFile('./app/data.json', JSON.stringify(todoData), function(err, data) {
+			if (err) throw err;
 			list.removeChild(e.target.parentNode);
 		})
 	}
-},false)
+}, false)
 
-let getIndex = (parent,ele,target) => {
-	if(!parent){
+let getIndex = (parent, ele, target) => {
+	if (!parent) {
 		var elements = document.querySelectorAll(ele);
-	}else{
+	} else {
 		var elements = parent.querySelectorAll(ele);
 	}
-	
-	for(let i=0,len=elements.length;i<len;i++){
-		if(elements[i]==target){
+
+	for (let i = 0, len = elements.length; i < len; i++) {
+		if (elements[i] == target) {
 			return i;
 		}
 	}
 	return -1;
 }
 
-addItem.addEventListener('click',function(){
+addItem.addEventListener('click', function() {
 	addItem.className += ' slideOutDown';
 	addArea.className += ' slideInUp';
-	addBg.className = addBg.className.replace('fn-hide','');
-},false);
+	addBg.className = addBg.className.replace('fn-hide', '');
+}, false);
 
 
-addArea.addEventListener('click',function(e){
+addArea.addEventListener('click', function(e) {
 	let addAreaCircle = addArea.querySelectorAll('.circle');
 	let input = addArea.querySelector('input');
 	let selectedPri = null;
-	if(e.target.className.indexOf('circle')>-1){
-		for(let i=0,len=addAreaCircle.length;i<len;i++){
-			if(addAreaCircle[i].className.indexOf('thin')==-1){
-				addAreaCircle[i].className += ' thin';				
+	if (e.target.className.indexOf('circle') > -1) {
+		for (let i = 0, len = addAreaCircle.length; i < len; i++) {
+			if (addAreaCircle[i].className.indexOf('thin') == -1) {
+				addAreaCircle[i].className += ' thin';
 			}
-			addAreaCircle[i].className = addAreaCircle[i].className.replace('selected','');
+			addAreaCircle[i].className = addAreaCircle[i].className.replace('selected', '');
 		}
-		e.target.className = e.target.className.replace('thin','');
+		e.target.className = e.target.className.replace('thin', '');
 		e.target.className += ' selected';
 	}
-	if(e.target.className.indexOf('button')>-1){
-		for(var i=0,len=addAreaCircle.length;i<len;i++){
-			if(addAreaCircle[i].className.indexOf('thin')==-1){
+	if (e.target.className.indexOf('button') > -1) {
+		for (var i = 0, len = addAreaCircle.length; i < len; i++) {
+			if (addAreaCircle[i].className.indexOf('thin') == -1) {
 				break;
 			}
 		}
-		if(i>=len){
+		if (i >= len) {
 			alert('要选择任务的优先级噢！');
-		}else{
-			if(input.value.trim()===''){
+		} else {
+			if (input.value.trim() === '') {
 				alert('任务内容不能为空');
-			}else{
+			} else {
 				selectedPri = addArea.querySelector('.selected').parentNode.className;
 
-				var lastIndex = getObjectIndexInArray(todoData,'priority',selectedPri);
-				console.log(lastIndex)
-				return;
+				var lastIndex = getObjectIndexInArray(todoData, 'priority', selectedPri);
 
-				todoData.push({"priority":selectedPri,"name":input.value.trim()});
-				
+				todoData.push({
+					"priority": selectedPri,
+					"name": input.value.trim()
+				});
+
 				todoData.sort(keysort('priority'));
-			
-				fs.writeFile('./app/data.json',JSON.stringify(todoData),function(err,data){
-					if(err) throw err;
-					for(let i=0,len=addAreaCircle.length;i<len;i++){
-						addAreaCircle[i].className = addAreaCircle[i].className.replace('selected','');
-						if(addAreaCircle[i].className.indexOf('thin')==-1){
-							addAreaCircle[i].className += ' thin';				
+
+				fs.writeFile('./app/data.json', JSON.stringify(todoData), function(err, data) {
+					if (err) throw err;
+					for (let i = 0, len = addAreaCircle.length; i < len; i++) {
+						addAreaCircle[i].className = addAreaCircle[i].className.replace('selected', '');
+						if (addAreaCircle[i].className.indexOf('thin') == -1) {
+							addAreaCircle[i].className += ' thin';
 						}
 					}
-					input.value='';
-					addBg.className += ' fn-hide';
 					var tmpNode = document.createElement('li');
-					tmpNode.className = 'item fadeInDown animated selectedPri';+selectedPri
-					tmpNode.innerHTML = '<i class="circle thin icon green"></i>' + input.value.trim();
-					li.insertBefore()
+					tmpNode.className = 'item fadeInDown animated pri'+ selectedPri;
+					tmpNode.innerHTML = '<i class="circle thin icon '+priColorMap[selectedPri]+'"></i>' + input.value.trim();
+					var referenceNode = list.querySelectorAll('.item')[lastIndex];
+					list.insertBefore(tmpNode,referenceNode ? referenceNode.nextSibling : referenceNode);
+
+					input.value = '';
+					addBg.className += ' fn-hide';
 				})
 			}
 		}
 	}
 });
 
-function getObjectIndexInArray(arr,key,value){
-	let count = 0;
-	console.log(arr,key,value)
-	for(var i=0,k=0,len=arr.length;i<len;i++){
+let priColorMap = {
+	'A':'red',
+	'B':'yellow',
+	'C':'blue',
+	'D':'green'
+}
+
+function getObjectIndexInArray(arr, key, value) {
+	let count = -1;
+	let equalFlag = false;
+	for (var i = arr.length - 1, len = 0; i >= len; i--) {
 		let tmpObj = arr[i];
-		if(tmpObj[key]&&tmpObj[key]!=value){
-			k++;
+		if (tmpObj[key] && tmpObj[key] == value) {
+			return i;
 		}
-		if(tmpObj[key]&&tmpObj[key]==value){
+	}
+	
+	if (i < len) {
+		switch (value) {
+			case 'A':
+				return 0;
+			case 'B':
+				return getObjectKeyDataLength(arr, 'priority', 'A')+getObjectKeyDataLength(arr, 'priority', 'B') - 1;
+			case 'C':
+				return getObjectKeyDataLength(arr, 'priority', 'A')+getObjectKeyDataLength(arr, 'priority', 'B')+getObjectKeyDataLength(arr, 'priority', 'C') - 1;
+			case 'D':
+				return getObjectKeyDataLength(arr, 'priority', 'A')+getObjectKeyDataLength(arr, 'priority', 'B')+getObjectKeyDataLength(arr, 'priority', 'C')+getObjectKeyDataLength(arr, 'priority', 'D') - 1 ;
+		}
+	}
+}
+
+function getObjectKeyDataLength(arr, key, value) {
+	let count = 0;
+	for (let i = 0, len = arr.length; i < len; i++) {
+		let tmpObj = arr[i];
+		if (tmpObj[key] && tmpObj[key] == value) {
 			count++;
 		}
 	}
-	console.log(count,k)
-	return count+k>=arr.length ? -1 : count+k;
+	return count;
 }
 
-function keysort(key,desc){
-	return function(a,b){
+function keysort(key, desc) {
+	return function(a, b) {
 		return desc ? ~~(a[key] < b[key]) : ~~(a[key] > b[key]);
 	}
 }
 
-ipcRenderer.on('global-shortcut',function(event,message){
+ipcRenderer.on('global-shortcut', function(event, message) {
 	let event = new MouseEvent('click');
-    soundButtons[message].dispatchEvent(event);
+	soundButtons[message].dispatchEvent(event);
 })
